ff1a27607ef3d5d0daddec5ab3115246edab14c6
Andrey Sokolov <sokolik_85@list.ru>
2017-06-10 12:13:48 +0700

    brain-even game

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 const readlineSync = require('readline-sync');
 
-export default () => {
-  console.log('Welcome to the Brain Games!');
+const NUMBER_OF_QUESTIONS = 3;
+const MIN = 1;
+const MAX = 20;
+
+const getListOfQuestions = (numOfQuestions, minValue, maxValue) => {
+  const questionsMap = [];
+  for (let i = 0; i < numOfQuestions; i += 1) {
+    const value = Math.floor(Math.random() * (maxValue - minValue)) + minValue;
+    questionsMap.push(value);
+  }
+  return questionsMap;
+};
+
+const brainGames = () => {
+  console.log(`
+  Welcome to the Brain Games!
+  Answer "yes" if number even otherwise answer "no".
+  `);
 
   const userName = readlineSync.question('May I have your name? ');
 
-  console.log(`\nHello, ${userName}!`);
+  const questionsMap = getListOfQuestions(NUMBER_OF_QUESTIONS, MIN, MAX);
+
+  for (const question of questionsMap) {
+    console.log('Question: ', question);
+    const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
+
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`Let's try again, ${userName}`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
+
+
+export default brainGames;
+
